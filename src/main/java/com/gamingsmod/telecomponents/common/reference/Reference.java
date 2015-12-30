119157319d47f88cfb6084836f0253d51dbfdd9f
Dominic Carrington <carringtondominic@gmail.com>
2015-12-30 15:15:06 +0000

    Fixed Server side proxy

--- src/main/java/com/gamingsmod/telecomponents/common/reference/Reference.java
+++ src/main/java/com/gamingsmod/telecomponents/common/reference/Reference.java
@@ -6,5 +6,5 @@ public class Reference
     public static final String MOD_NAME = "TeleComponents";
     public static final String VERSION = "1.7.10-0.1.0";
     public static final String CLIENT_SIDE_PROXY = "com.gamingsmod.telecomponents.client.ClientProxy";
-    public static final String SERVER_SIDE_PROXY = "com.gamingsmod.telecomponents.CommonProxy";
+    public static final String SERVER_SIDE_PROXY = "com.gamingsmod.telecomponents.common.CommonProxy";
 }
