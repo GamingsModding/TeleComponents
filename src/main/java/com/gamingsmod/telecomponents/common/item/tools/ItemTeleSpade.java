bbc3f6fac9a3f2d6cacd0e5ded3710d0f88bdb56
Dominic Carrington <carringtondominic@gmail.com>
2016-02-10 20:38:21 +0000

    Typos....

--- src/main/java/com/gamingsmod/telecomponents/common/item/tools/ItemTeleSpade.java
+++ src/main/java/com/gamingsmod/telecomponents/common/item/tools/ItemTeleSpade.java
@@ -13,12 +13,12 @@ public class ItemTeleSpade extends ItemToolTeleC
     private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(new Block[] {Blocks.clay, Blocks.dirt, Blocks.farmland, Blocks.grass, Blocks.gravel, Blocks.mycelium, Blocks.sand, Blocks.snow, Blocks.snow_layer, Blocks.soul_sand});
 
     public ItemTeleSpade() {
-        super("teleIngotSpace", 1.0F, Material.Tools.TELEINGOT, EFFECTIVE_ON);
+        super("teleIngotSpade", 1.0F, Material.Tools.TELEINGOT, EFFECTIVE_ON);
     }
 
     // From ItemSpade
     public boolean canHarvestBlock(Block blockIn)
     {
-        return blockIn == Blocks.snow_layer ? true : blockIn == Blocks.snow;
+        return blockIn == Blocks.snow_layer || blockIn == Blocks.snow;
     }
 }
