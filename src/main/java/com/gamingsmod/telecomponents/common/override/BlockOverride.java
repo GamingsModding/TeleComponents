3a9c22106e564bbccf5e8f572c88e93b1d36e5c6
Dominic Carrington <carringtondominic@gmail.com>
2016-02-11 20:43:54 +0000

    Fixed dropping obsidian chunks

--- src/main/java/com/gamingsmod/telecomponents/common/override/BlockOverride.java
+++ src/main/java/com/gamingsmod/telecomponents/common/override/BlockOverride.java
@@ -1,19 +1,27 @@
 package com.gamingsmod.telecomponents.common.override;
 
 import com.gamingsmod.telecomponents.common.init.ModItems;
+import net.minecraft.block.BlockObsidian;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Random;
 
 public class BlockOverride
 {
     @SubscribeEvent
-    public void onDrops(BlockEvent.HarvestDropsEvent event)
+    public void onDrops(BlockEvent.HarvestDropsEvent e)
     {
-        if (event.drops == Blocks.obsidian) {
-            event.drops.clear();
-            event.drops.add(new ItemStack(ModItems.obsidianChunk));
+        if (e.state.getBlock() instanceof BlockObsidian) {
+            if (!e.isSilkTouching) {
+                e.drops.clear();
+                Random rand = new Random();
+                ItemStack stack = new ItemStack(ModItems.obsidianChunk, (rand.nextInt(4) + 1)*(e.fortuneLevel + 1));
+                e.drops.add(stack);
+            }
         }
     }
 
